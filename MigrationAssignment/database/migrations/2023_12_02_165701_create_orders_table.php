24799d5387135dc5d123c1d24d93f6ace7f1941f
ssmugdho7 <shahmarufsiraj@gmail.com>
2023-12-02 23:25:00 +0600

    first comment

---
+++ MigrationAssignment/database/migrations/2023_12_02_165701_create_orders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_ID')->references('id')->on('products')->cascadeOnUpdate()->restrictOnDelete();
+           
+            $table->string('quantity');
+            $table->timestamp("created_at")->useCurrent();
+            $table->timestamp("upadated_at")->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
